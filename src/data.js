fac84c9ecd4b046a6781de2ffc11863687a17c8c
lxsca00 <ljimena10@gmail.com>
2022-06-14 12:57:10 -0500

    mostrar las cartas de pokemon

--- src/data.js
+++ src/data.js
@@ -1,11 +1,15 @@
 // estas funciones son de ejemplo
 
-export const allPokemon = (data) => {
-  const pokemon = []
-  for (let i=0; i<data.length; i++){
-    pokemon.push(data[i]);
-  }
-  return pokemon
+//import pokemon from "./data/pokemon/pokemon";
+
+export const pokemonCard = (pokemon) => {
+  const {img, num, name} = pokemon
+    return `
+    <figure class="onePokemon">
+        <img alt="This is a pokemon" src="${img}"><br>
+        <span> ${num} </span><br>
+        <span> ${name} </span><br>
+      </figure>`
 }
 
 export const example = () => {
