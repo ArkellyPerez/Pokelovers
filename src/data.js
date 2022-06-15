7e1b6889f82b2e3d89c726c5dd77805e360cce58
ArkellyPerez <arkelly.perez.alayo@gmail.com>
2022-06-15 12:12:35 -0500

    Primera vista

--- src/data.js
+++ src/data.js
@@ -1,5 +1,18 @@
 // estas funciones son de ejemplo
 
+//import pokemon from "./data/pokemon/pokemon";
+
+export const pokemonCard = (pokemon) => {
+  const {img, num, name} = pokemon
+    return `
+    <figure class="onePokemon">
+        <img alt="This is a pokemon" src="${img}"><br>
+        <span> ${num} </span><br>
+        <span> ${name} </span><br>
+      </figure>`
+}
+
+
 export const example = () => {
   return 'example';
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,44 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { pokemonCard } from './data.js';
 
-console.log(example, data);
+import data from './data/pokemon/pokemon.js';
+
+//____________________________________me_________________________________________________________
+let ButtonInicio=document.getElementById('goPokedex');
+document.getElementById("pokedex").style.display = "none";
+
+ButtonInicio.addEventListener("click", (e) => {
+    e.preventDefault(); //cancela el evento por defecto
+    document.getElementById("Form-Welcome").style.display = "none";
+    document.getElementById("pokedex").style.display = "block";
+    
+})
+
+
+//_____________________________________________________________________________________________
+
+const allPokemon = data.pokemon //Data de todos los pokemon y características
+
+const pokemonCards = allPokemon.map((pokemon)=>pokemonCard(pokemon))
+
+const container = document.querySelector('.pokemonContainer');
+container.innerHTML = pokemonCards.join('')
+
+//let numberAndName = allPokemon.map(whatPokemon) Devuelve todos los números y nombres en un array de strings
+
+//document.getElementById('print').innerHTML=numberAndName //Imprime numberAndName en el id 'print'
+
+/*function whatPokemon (pokemon){ //Función para iterar y devolver ciertos elementos de la data, devuelve string
+    return pokemon.num + ' ' + pokemon.name
+}*/
+
+console.log(pokemonCards)
+/*const pokemonFirstGen = pokemon.filter(p => p.num <= 151);
+
+const pokemonSecondGen = pokemon.filter(p => p.num > 151 && p.num <=251)*/
+
+
+
+
+//}
+
+
