b1ae178920d64fb3523b25b20724fb250da73805
FiorellaCole <fiorellacoleaguilar@gmail.com>
2022-06-16 10:58:05 -0500

    status actual

--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ import { pokemonCard, filterByType } from './data.js';
 
 import data from './data/pokemon/pokemon.js';
 
+
+
+let ButtonInicio=document.getElementById('goPokedex');
+document.getElementById("pokedex").style.display = "none";
+
+ButtonInicio.addEventListener("click", (e) => {
+    e.preventDefault(); //cancela el evento por defecto
+    document.getElementById("Form-Welcome").style.display = "none";//Oculta la primera vista
+    document.getElementById("pokedex").style.display = "block"; //Muestra la segunda vista  
+})
+
+
+
 const allPokemon = data.pokemon //Data de todos los pokemon y características
 
 const pokemonCards = allPokemon.map(function(pokemon){ 
@@ -12,6 +25,12 @@ const container = document.querySelector('.pokemonContainer');
 container.innerHTML = pokemonCards.join('')
 
 const pokemonTypes = ['poison', 'grass', 'fire']
+
+
+
+
+
+
 
 /*let pokePoison = allPokemon.filter(function(pokemon){
     if (pokemon.type.includes('poison')){
