070ecbdaf1f2a429cbaf7fe178fa93f7a9a5197a
lxsca00 <ljimena10@gmail.com>
2022-06-15 19:13:54 -0500

    función filtrado

--- src/main.js
+++ src/main.js
@@ -1,29 +1,29 @@
-import { pokemonCard } from './data.js';
-
+import { pokemonCard, filterByType } from './data.js';
 
 import data from './data/pokemon/pokemon.js';
 
-
 const allPokemon = data.pokemon //Data de todos los pokemon y características
 
-const pokemonCards = allPokemon.map((pokemon)=>pokemonCard(pokemon))
-
+const pokemonCards = allPokemon.map(function(pokemon){ 
+    return pokemonCard(pokemon)
+})
+//console.log(pokemonCards)
 const container = document.querySelector('.pokemonContainer');
 container.innerHTML = pokemonCards.join('')
 
-//let numberAndName = allPokemon.map(whatPokemon) Devuelve todos los números y nombres en un array de strings
 
-//document.getElementById('print').innerHTML=numberAndName //Imprime numberAndName en el id 'print'
+const pokemonTypes = ['poison', 'grass', 'fire']
 
-/*function whatPokemon (pokemon){ //Función para iterar y devolver ciertos elementos de la data, devuelve string
-    return pokemon.num + ' ' + pokemon.name
-}*/
+/*let pokePoison = allPokemon.filter(function(pokemon){
+    if (pokemon.type.includes('poison')){
+         return pokemon
+    }
+})*/
 
 
-console.log(pokemonCards)
-/*const pokemonFirstGen = pokemon.filter(p => p.num <= 151);
+console.log(filterByType(allPokemon, pokemonTypes[1]))
 
-const pokemonSecondGen = pokemon.filter(p => p.num > 151 && p.num <=251)*/
+//const pokemonSecondGen = pokemon.filter(p => p.num > 151 && p.num <=251)
 
 
 
