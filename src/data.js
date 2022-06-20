52fb27d83484edd4160a60199cda48e6a679ccd7
FiorellaCole <fiorellacoleaguilar@gmail.com>
2022-06-20 09:10:49 -0500

    función búsqueda por nombre

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
-export const pokemonCard = function(pokemon) {
-  const {img, num, name} = pokemon
-    return `
+export const pokemonCard = function (pokemon) {
+  const { img, num, name } = pokemon
+  return `
     <figure class="onePokemon">
         <p id="pokemonNumber"> ${num} </p>
         <p id="pokemonName"> ${name} </p>
@@ -8,12 +8,12 @@ export const pokemonCard = function(pokemon) {
       </figure>`
 }
 
-export const filterByType = function (allPokemon,type) {
-  let pokemonByType = allPokemon.filter(function(pokemon){
-    if (pokemon.type.includes(type)){
-         return pokemon
-      }
-    })
+export const filterByType = function (allPokemon, type) {
+  let pokemonByType = allPokemon.filter(function (pokemon) {
+    if (pokemon.type.includes(type)) {
+      return pokemon
+    }
+  })
   return pokemonByType
 }
 
@@ -21,20 +21,28 @@ export const filterByType = function (allPokemon,type) {
   return pokemonCard(pokemon)
 })*/
 
-export const pokeArr = function(array){
-  let newCards = array.map(function(pokemon){
+export const pokeArr = function (array) {
+  let newCards = array.map(function (pokemon) {
     return pokemonCard(pokemon)
   })
   return newCards
 }
 
 export const sortByName = function (selectionTypeArray1) {
-  let sortByNamePokemon=selectionTypeArray1.sort(function(a,b){
-    
-    if (a.name < b.name) {return -1;}
-    if (a.name > b.name) {return 1;}
+  let sortByNamePokemon = selectionTypeArray1.sort(function (a, b) {
+
+    if (a.name < b.name) { return -1; }
+    if (a.name > b.name) { return 1; }
     return 0;
   });
 
   return sortByNamePokemon;
+}
+
+
+export const pokeSearch = (allPokemon,name) => {
+  let searchPokeName=allPokemon.filter(function(pokemon) {
+    return pokemon.name.startsWith(name);
+  })
+  return searchPokeName;
 }
